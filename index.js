becb094d4a8336fe7ba25a8602e5c6ee052ea242
Mac Heller-Ogden <mac@machellerogden.com>
2019-02-07 15:20:49 -0600

    better debugability

--- index.js
+++ index.js
@@ -1,70 +1,72 @@
 'use strict';
 
-const Sym = Symbol('@mediary');
+const PatchSymbol = Symbol('@mediary');
 
 module.exports = mediary;
 
 function mediary(given) {
-    if (['string', 'number', 'boolean'].includes(typeof given)) return given;
+    if ([ 'string', 'number', 'boolean' ].includes(typeof given)) return given;
     if (![ '[object Object]', '[object Array]' ].includes(Object.prototype.toString.call(given))) throw new TypeError(`Given value must be a simple object. Received: ${given}`);
-    if (given[Sym]) return given;
-    given[Sym] = true;
+    if (given[PatchSymbol]) return given;
     const mediated = Array.isArray(given)
         ? given.reduce((acc, v, i) => (acc[i] = mediary(v), acc), [])
         : Object.entries(given).reduce((acc, [k, v]) => (acc[k] = mediary(v), acc), {});
-    const next = Array.isArray(given)
+    const patch = Array.isArray(given)
         ? []
         : {};
     const deletions = new Set();
     const handler = {
         defineProperty(target, key, attr) {
-            return Reflect.defineProperty(next, key, attr);
+            return Reflect.defineProperty(patch, key, attr);
         },
         deleteProperty(target, key) {
             deletions.add(key);
-            return Reflect.deleteProperty(next, key);
+            return Reflect.deleteProperty(patch, key);
         },
         isExtensible(target) {
-            return Reflect.isExtensible(next);
+            return Reflect.isExtensible(patch);
         },
         preventExtensions(target) {
-            return Reflect.preventExtensions(next);
+            return Reflect.preventExtensions(patch);
         },
         get (target, key) {
-            if (next[key] != null) return Reflect.get(next, key);
+            if (key === PatchSymbol) return patch;
+            if (patch[key] != null) return Reflect.get(patch, key);
             if (deletions.has(key)) return void 0;
             return Reflect.get(...arguments);
         },
         getOwnPropertyDescriptor (target, key) {
-            if (deletions.has(key)) return Reflect.getOwnPropertyDescriptor(next, key);
-            return Reflect.getOwnPropertyDescriptor(Array.isArray(next)
-                ? [ ...target, ...next ]
-                : { ...target, ...next }, key);
+            if (deletions.has(key)) return Reflect.getOwnPropertyDescriptor(patch, key);
+            return Reflect.getOwnPropertyDescriptor(Array.isArray(patch)
+                ? [ ...target, ...patch ]
+                : { ...target, ...patch }, key);
         },
         // TODO: needs consideration
         //getPrototypeOf (target) {
-            //return Reflect.getPrototypeOf(next);
+            //return Reflect.getPrototypeOf(patch);
         //},
         //setPrototypeOf (target, prototype) {
-            //return Reflect.getPrototypeOf(next, prototype);
+            //return Reflect.getPrototypeOf(patch, prototype);
         //},
         ownKeys (target) {
             const withDeletionsOmitted = Object.entries(target).reduce((acc, [k, v]) => {
                 if (!deletions.has(k)) acc[k] = v;
                 return acc;
-            }, Array.isArray(next) ? [] : {});
-            return Reflect.ownKeys(Array.isArray(next)
-                ? [ ...withDeletionsOmitted, ...next ]
-                : { ...withDeletionsOmitted, ...next })
+            }, Array.isArray(patch) ? [] : {});
+            return Reflect.ownKeys(Array.isArray(patch)
+                ? [ ...withDeletionsOmitted, ...patch ]
+                : { ...withDeletionsOmitted, ...patch })
         },
         has (target, key) {
-            if (key in next) return true;
+            if (key in patch) return true;
             if (deletions.has(key)) return false;
             return Reflect.has(target, key);
         },
         set (target, key, value) {
-            return Reflect.set(next, key, value);
+            return Reflect.set(patch, key, value);
         }
     };
     return new Proxy(mediated, handler);
 }
+
+mediary.PatchSymbol = PatchSymbol;
--- sandbox.js
+++ sandbox.js
@@ -1,5 +1,6 @@
 'use strict'
 const mediary = require('.');
+const { PatchSymbol } = mediary;
 const {inspect} = require('util');
 const log = (...args) => console.log(inspect(args, { depth: null, colors: true }));
 const foo = {
@@ -18,3 +19,4 @@ log('bar', bar);
 bar.a.b[0].c = 'z';
 log('foo', foo);
 log('bar', bar);
+log('patch', bar.a.b[0][PatchSymbol]);
